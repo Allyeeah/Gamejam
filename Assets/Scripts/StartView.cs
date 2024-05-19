632023379bdcc87811396247beda560b3a25f7a7
Allyeeah <109127333+Allyeeah@users.noreply.github.com>
2024-05-20 00:19:28 +0900

    cardkey&startView시작버튼작동

--- Assets/Scripts/StartView.cs
+++ Assets/Scripts/StartView.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
 
 public class StartView : MonoBehaviour
 {
@@ -14,4 +15,5 @@ public class StartView : MonoBehaviour
         }
         
     }
+
 }
