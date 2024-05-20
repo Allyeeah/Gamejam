31e5026462c544206bdd2f713fc1d3391acd40f6
Allyeeah <109127333+Allyeeah@users.noreply.github.com>
2024-05-21 00:41:30 +0900

    최상층 긴복도 background

--- Assets/Scripts/QuitButton.cs
+++ Assets/Scripts/QuitButton.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static System.Net.Mime.MediaTypeNames;
+//using static System.Net.Mime.MediaTypeNames;
 
 public class QuitButton : MonoBehaviour
 {
@@ -10,12 +10,12 @@ public class QuitButton : MonoBehaviour
     public void QuitGame()
     {
         // 에디터에서 실행 중일 때는 게임이 종료되지 않기 때문에 에디터인지 확인
-#if UNITY_EDITOR
-        // UnityEditor.EditorApplication.isPlaying을 false로 설정하여 에디터 실행을 중지합니다.
+    #if UNITY_EDITOR
+        // UnityEditor.EditorApplication.isPlaying을 false로 설정하여 에디터 실행을 중지
         UnityEditor.EditorApplication.isPlaying = false;
-#else
+    #else
         // 빌드된 게임에서는 Application.Quit()을 호출하여 게임을 종료
         Application.Quit();
-#endif
+    #endif
     }
 }
